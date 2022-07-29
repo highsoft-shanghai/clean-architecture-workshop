9133e7727600b29dca9f3011b8f589a95a5bd5c8
Neil Wang <wangweili457@gmail.com>
2022-07-29 10:55:30 +0800

    DONE: Add salarying modules.

---
+++ modules/salarying.gateways/src/test/java/workshop/architecture/clean/salarying/gateways/CanaryTest.java
@@ -0,0 +1,14 @@
+package workshop.architecture.clean.salarying.gateways;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CanaryTest {
+
+    @Test
+    void should_run_tests() {
+        assertEquals("ok", "ok");
+    }
+
+}
