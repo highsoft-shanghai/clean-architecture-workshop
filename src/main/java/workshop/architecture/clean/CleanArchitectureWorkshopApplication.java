204471c78fbed7ea0e87e6584e79395cb97af87f
Neil Wang <wangweili457@gmail.com>
2022-07-29 12:22:55 +0800

    DONE: Spring cloud.

--- src/main/java/workshop/architecture/clean/CleanArchitectureWorkshopApplication.java
+++ src/main/java/workshop/architecture/clean/CleanArchitectureWorkshopApplication.java
@@ -2,8 +2,12 @@ package workshop.architecture.clean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
 public class CleanArchitectureWorkshopApplication {
 
     public static void main(String[] args) {
